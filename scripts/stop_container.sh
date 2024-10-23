796ed2a258e8f13c0ec33facd527dd288644be13
Sumanth <sumanthkumar1298@gmail.com>
2024-10-23 15:44:33 +0530

    Script modification

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 set -e
 
-# Stop the running container (if any)
-container_id=`docker ps | awk -f " " '{print $1}'`
-docker -rm -f $container_id
+
+# Fetch the latest container ID
+container_id=$(docker ps -ql)
+
+# Check if there's any container to remove
+if [ -n "$container_id" ]; then
+  echo "Stopping and removing the latest container with ID: $container_id"
+
+  # Stop the container
+  docker stop $container_id
+  
+  # Remove the container
+  docker rm $container_id
+
+  echo "Container $container_id has been removed."
+else
+  echo "No running containers found."
+fi
